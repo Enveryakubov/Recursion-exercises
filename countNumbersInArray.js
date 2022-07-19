e6028b4ecacf982cc5d2f47c577ca4815e46e2f8
Enver Yakubov <enveryakubov@gmail.com>
2022-07-19 23:01:52 +0300

    refactored solution

--- countNumbersInArray.js
+++ countNumbersInArray.js
@@ -1,16 +1,14 @@
 const countNumbers = (arr, count = 0) => {
   if (arr.length === 0) return count;
-  let last = arr.pop();
-  if (typeof last === 'number') {
-    ++count;
-    return countNumbers(arr, count);
-  } else if (Array.isArray(last)) {
+  const last = arr.pop();
+  if (Array.isArray(last)) {
     return countNumbers([...arr, ...last], count);
   } else {
+    typeof last === 'number' && ++count;
     return countNumbers(arr, count);
   }
 };
 
 // console.log(
-//   countNumbers([1, 2, [4], [[[[[45, 5656]]]]], 'sdsd', [[[['4', 7]]]]])
+//   countNumbers([1, 2, 3, [4], [[[[[45, 5656]]]]], 'sdsd', [[[['4', 7]]]]])
 // );
